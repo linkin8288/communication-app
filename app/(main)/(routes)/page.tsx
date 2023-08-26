c6ea5e8ec7485633b30505684e984728de32734c
charlie <linkin8288@gmail.com>
2023-08-26 15:13:36 +0200

    Add Dark Mode button

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,8 +1,13 @@
+// ModeToggle is the dark mode toggle
+
+import { ModeToggle } from "@/components/mode-toggle";
 
 export default function Home() {
   return (
     <div>
       This is a protected route.
+      <ModeToggle/>
     </div>
+    
   )
 }
